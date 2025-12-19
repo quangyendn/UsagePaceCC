05b75829ddac340947e705c77f5c182b26454cea
f-is-h <poemfar@gmail.com>
2025-12-19 09:36:01 +0900

    tests: Cloudflare function

--- website/functions/_middleware.js
+++ website/functions/_middleware.js
@@ -14,11 +14,21 @@ export async function onRequest(context) {
   const realAddress = context.env.REAL_ADDRESS || '[ADDRESS_PLACEHOLDER]';
 
   let html = await response.text();
+
+  const hasPlaceholder = html.includes('[ADDRESS_PLACEHOLDER');
+  const envVarExists = !!context.env.REAL_ADDRESS;
+
   html = html.replace(/\[ADDRESS_PLACEHOLDER[^\]]*\]/g, realAddress);
 
-  return new Response(html, {
+  const newResponse = new Response(html, {
     status: response.status,
     statusText: response.statusText,
     headers: response.headers
   });
+
+  newResponse.headers.set('X-Debug-Placeholder-Found', hasPlaceholder ? 'yes' : 'no');
+  newResponse.headers.set('X-Debug-Env-Var-Exists', envVarExists ? 'yes' : 'no');
+  newResponse.headers.set('X-Debug-Env-Value-Length', context.env.REAL_ADDRESS ? context.env.REAL_ADDRESS.length : '0');
+
+  return newResponse;
 }
